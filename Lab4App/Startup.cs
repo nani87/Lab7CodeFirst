ad969483fd0efcd2c536237d02c10a67c767713c
anacorrea87 <anacorrea86@hotmail.com>
2017-10-03 20:29:56 -0700

    Add project files.

---
+++ Lab4App/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab4App.Startup))]
+namespace Lab4App
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
